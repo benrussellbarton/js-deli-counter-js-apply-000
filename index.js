175e16e8391f662422496ec556b822b17612ae34
Ben Barton <benrussellbarton@gmail.com>
2019-05-06 10:33:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function currentLine(katzDeliLine) {
   }
   let currentLine = "The line is currently:";
   for (let i = 0; i < katzDeliLine.length; i++){
-    currentLine += ` ${i + 1}. ${katzDeliLine[i]},`
+    currentLine += ` ${i + 1}. ${katzDeliLine[i].join(', ')},`
   }
   return currentLine;
 }
