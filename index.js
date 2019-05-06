f8ad497a6d65431af5de11c363200b5897b21dbd
Ben Barton <benrussellbarton@gmail.com>
2019-05-06 11:00:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function nowServing(katzDeliLine) {
   if (!katzDeliLine.length) {
     return "There is nobody waiting to be served!";
   }
-  console.log(`Currently serving ${katzDeliLine[0]}.`);
-  return katzDeliLine.shift();
+  return `Currently serving ${katzDeliLine.shift()}.`;
 }
 
 //Build a function currentLine that accepts the current line of people and returns the current line as a string; for example, if 'katzDeliLine' is currently ["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is currently: 1. Ada, 2. Grace". You don't have to use katzDeliLine as a variable or parameter name in your function though, it's just an example of a variable that might be passed to it. If there is nobody in line, it should return "The line is currently empty."
